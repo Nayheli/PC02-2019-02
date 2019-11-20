319e5ccb8b93386f9ec3fee47b32807f8c6b85ac
Nayheli Cahuaza <nayheli.alexandra09@gmail.com>
2019-11-20 08:36:30 -0500

    cambio2

--- pregunta3.py
+++ pregunta3.py
@@ -5,7 +5,7 @@ sexo = input("¿Que sexo es ?")
 edad = int(input("¿Cuantos años tiene?: ")) 
 while sexo=="femenino" or sexo=="masculino":
     print("Error,por favor escriba de nuevo")
-    sexo = input("¿Que sexo es ?")
+    sexo = input("¿Que sexo es :?")
 
 while edad<=5 and edad>=17:
     print("Error,por favor escriba de nuevo")
