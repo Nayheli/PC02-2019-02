290a6184084d949a3f0588eb737ceb3f6d528c5d
Nayheli Cahuaza <nayheli.alexandra09@gmail.com>
2019-11-20 08:31:53 -0500

    cambio1

---
+++ pregunta2.py
@@ -0,0 +1,6 @@
+def contarletra(texto, letra):
+    texto = input("Escribir la palabra que desee:")
+    letra = input("¿Que letra desea contar")
+
+if texto =="":
+    print(letra)
---
+++ pregunta3.py
@@ -0,0 +1,18 @@
+
+
+nombre = input("¿Cual es su nombre?: ")
+sexo = input("¿Que sexo es ?")
+edad = int(input("¿Cuantos años tiene?: ")) 
+while sexo=="femenino" or sexo=="masculino":
+    print("Error,por favor escriba de nuevo")
+    sexo = input("¿Que sexo es ?")
+
+while edad<=5 and edad>=17:
+    print("Error,por favor escriba de nuevo")
+    edad = int(input("¿Cuantos años tiene?: "))    
+
+
+print("------Lista de inscritos------")
+print(nombre)
+print(sexo)
+print(edad)        
