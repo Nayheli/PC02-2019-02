a091de89c33bad21cd15308966193e5457b11566
Nayheli Cahuaza <nayheli.alexandra09@gmail.com>
2019-11-20 08:39:01 -0500

    cambio3

--- pregunta4.py
+++ pregunta4.py
@@ -1,6 +1,6 @@
 import random
 
-n = int(input("Ingrese el numero del tamaño de la matriz: "))
+n = int(input("Ingrese el numero del tamaño de la matriz que desee: "))
 while n >=2 and n<=5:
     for i in range (0,101):
         print(i)
